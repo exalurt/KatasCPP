30f5fdf081c1fe0340f81a7b9b903a6ebc8b4a5d
App PSOE Gáldar <appgaldarpsoe@gmail.com>
2015-07-08 09:15:22 +0100

    Refactorizando fizzbuzz

--- FizzBuzz/FizzBuzz.cpp
+++ FizzBuzz/FizzBuzz.cpp
@@ -6,23 +6,43 @@
 #include <string>
 using namespace std;
 
-string divisible3(const int num)
+bool contain(string value, int num)
 {
-	if (0 == num % 3 || string::npos != to_string(num).find("3"))
+	return string::npos != to_string(num).find(value);
+}
+
+bool isDivisible(int divisor, int num)
+{
+	return (0 == num % divisor);
+}
+
+bool isBuzz(int num)
+{
+	return isDivisible(5, num) || contain("5", num);
+}
+
+bool isFizz(int num)
+{
+	return isDivisible(3, num) || contain("3", num);
+}
+
+string Fizz(const int num)
+{
+	if (isFizz(num))
 		return string{ "Fizz" };
 	return "";
 }
 
-string divisible5(const int num)
+string Buzz(const int num)
 {
-	if (0 == num % 5 || string::npos != to_string(num).find("5"))
+	if (isBuzz(num))
 		return string{ "Buzz" };
 	return "";
 }
 
 string CFizzBuzz::FizzBuzz(const int num)
 {
-	string result = divisible3(num) + divisible5(num);
+	string result = Fizz(num) + Buzz(num);
 
-	return (0 != result.length() ? result : to_string(num));
+	return result.empty() ? to_string(num) : result;
 }
